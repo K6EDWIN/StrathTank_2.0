fa947fa56e4f2c629c9d913e34715471840283bf
Sharon <sharon.kanyi@strathmore.edu>
2025-10-18 13:49:54 +0300

    testing

---
+++ src/main/java/com/strathtank/app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.strathtank.app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        
+        button.setOnClickListener {
+            textView.text = "Welcome to StrathTank 2.0!"
+        }
+    }
+}
